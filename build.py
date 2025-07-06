a88f47cd0a51540b7512e58a1a411fb521a971ab
y4n2sTh3Cod3r <Ya55mm55mm23@gmail.com>
2025-07-06 20:16:03 +0100

    Fixed build script to build one lib file

--- build.py
+++ build.py
@@ -3,28 +3,40 @@ import os
 from glob import glob
 from setuptools import setup, Extension
 from Cython.Build import cythonize
+import subprocess
 
 # Collect all pyx modules under src/
-pyx_files = glob(os.path.join("src", "**", "*.pyx"), recursive=True)
+def build():
+    pyx_files = glob(os.path.join("src", "**", "*.pyx"), recursive=True)
 
-# Create one Extension per file
-exts = [
-    Extension(
-        os.path.splitext(os.path.relpath(path, "src"))[0].replace(os.sep, "."),
-        [path],
-        language="c++",
-        include_dirs=["src"],
+    # Create one Extension per file
+    exts = [
+        Extension(
+            "runtime",
+            sources=pyx_files,
+            language="c++",
+            include_dirs=["src"],
+        )
+    ]
+
+    setup(
+        name="embercore_runtime",
+        version="0.1",
+        ext_modules=cythonize(
+            exts,
+            compiler_directives={"language_level": "3"},
+            annotate=False
+        ),
+        zip_safe=False,
     )
-    for path in pyx_files
-]
 
-setup(
-    name="embercore_runtime",
-    version="0.1",
-    ext_modules=cythonize(
-        exts,
-        compiler_directives={"language_level": "3"},
-        annotate=False,
-    ),
-    zip_safe=False,
-)
+    genProc = subprocess.Popen(
+        "cd EmberCoreRuntime && stubgen",
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+
+    genProc.wait()
+
+build()
